05f3b89bcd35399b71355339e210f88dcfb3c42a
Yixiang Huang <huangyixiang.qwehc@gmail.com>
2017-09-05 10:41:57 +0800

    Add Datum which is data structure for a document. Add DataReader which is corresponded to Datums.

--- Preprocessing/DataReader.py
+++ Preprocessing/DataReader.py
@@ -2,5 +2,24 @@ from Datum import Datum
 
 
 class DataReader(object):
-    def read(self):
+    def __init__(self, **kw):
+        self.Datums = []
+        self.source_data_file = kw.get('source_data_file')
+
+    def standard_read(self):
+        with open(self.source_data_file) as fopen:
+            doc_id = 0
+            tokens, glabels = [], []
+            for line in fopen.readlines():
+                if line.strip():
+                    tokens.append(line.strip().split(' ')[0])
+                    glabels.append(line.strip().split(' ')[1])
+                elif line.strip() == '' and tokens:
+                    doc = ' '.join(tokens)
+                    datum = Datum(doc_id, doc, tokens, glabels, [])
+                    self.Datums.append(datum)
+                    doc_id += 1
+                    tokens, glabels = [], []
+
+    def add_predict_labels(self, labels):
         pass
--- Preprocessing/Datum.py
+++ Preprocessing/Datum.py
@@ -2,15 +2,19 @@ class Datum(object):
     '''
     Do Refactor Here.
     '''
-    def __init__(self):
-        self.doc_id = None
-        self.doc = None
-        self.doc_token_id = None
-        self.doc_token = None
-        self.doc_golden_label = None
-        self.token_glabel_map = None
-        self.doc_label = None
-        self.token_label_map = None
 
-    def get_sentence(self):
-        pass
+    def __init__(self, doc_id, doc, tokens, glabels, labels):
+        self.doc_id = doc_id
+        self.doc = doc
+        self.tokens = tokens
+        self.golden_labels = glabels
+        self.labels = labels
+
+    def get_sentence(self, index_token):
+        return self.doc
+
+    def get_golden_labels(self, index_golden_label):
+        return self.golden_labels
+
+    def get_labels(self, index_labels):
+        return self.labels
