33f10b307dc9763455c2658bf8fbd6cc6d76a0cc
Yixiang Huang <huangyixiang.qwehc@gmail.com>
2017-09-07 11:11:08 +0800

    Refactor:Analysis scripts, add basic class for all scripts.

---
+++ Scripts/ResultAnalysisBasic.py
@@ -0,0 +1,15 @@
+from Scripts.ScriptToolkit import ScriptToolkit
+
+
+class ResultAnalysisBasic(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        print 'This is an analyzer, start analyzing...'
+
+    def run(self):
+        pass
+
+    def stop(self):
+        pass
